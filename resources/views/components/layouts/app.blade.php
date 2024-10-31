62f58472623943899ba9aa2e6e0e7d85cd4eccd0
Dasse <pascaldasse56@gmail.com>
2024-10-31 13:29:41 +0100

    sidebar

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -30,42 +30,19 @@
 <livewire:navigation.navbar  />
 
     {{-- MAIN --}}
-    <x-main full-width>
-        {{-- SIDEBAR --}}
-        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
+  <x-main full-width>
 
-            {{-- BRAND --}}
-            <x-app-brand class="p-5 pt-3" />
+    {{-- SIDEBAR --}}
+    <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit lg:hidden">
+        <livewire:navigation.sidebar />
+    </x-slot:sidebar>
 
-            {{-- MENU --}}
-            <x-menu activate-by-route>
+    {{-- SLOT --}}
+    <x-slot:content>
+        {{ $slot }}
+    </x-slot:content>
 
-                {{-- User --}}
-                @if($user = auth()->user())
-                    <x-menu-separator />
-
-                    <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
-                        <x-slot:actions>
-                            <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
-                        </x-slot:actions>
-                    </x-list-item>
-
-                    <x-menu-separator />
-                @endif
-
-                <x-menu-item title="Hello" icon="o-sparkles" link="/" />
-                <x-menu-sub title="Settings" icon="o-cog-6-tooth">
-                    <x-menu-item title="Wifi" icon="o-wifi" link="####" />
-                    <x-menu-item title="Archives" icon="o-archive-box" link="####" />
-                </x-menu-sub>
-            </x-menu>
-        </x-slot:sidebar>
-
-        {{-- The `$slot` goes here --}}
-        <x-slot:content>
-            {{ $slot }}
-        </x-slot:content>
-    </x-main>
+</x-main>
 
     {{--  TOAST area --}}
     <x-toast />
