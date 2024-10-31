72fef046b9bbde70acad89319860cc08a8de51c5
Dasse <pascaldasse56@gmail.com>
2024-10-31 13:19:54 +0100

    add photos

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 
-Volt::route('/', 'users.index');
+Volt::route('/', 'index');
 Route::middleware('guest')->group(function () {
     Volt::route('/login', 'auth.login')->name('login');
     Volt::route('/register', 'auth.register');
