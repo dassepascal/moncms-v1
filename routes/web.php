b56b8e1750a3e5b7ab064925daa91753ae5e44eb
Dasse <pascaldasse56@gmail.com>
2024-10-31 13:17:33 +0100

    init auth

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,7 @@ Volt::route('/', 'users.index');
 Route::middleware('guest')->group(function () {
     Volt::route('/login', 'auth.login')->name('login');
     Volt::route('/register', 'auth.register');
+    Volt::route('/forgot-password', 'auth.forgot-password');
+    Volt::route('/reset-password/{token}', 'auth.reset-password')->name('password.reset');
  
 });
