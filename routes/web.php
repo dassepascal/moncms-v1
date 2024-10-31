7684af85f60e2337f84a65d7a7fef3df56b0c0e4
Dasse <pascaldasse56@gmail.com>
2024-10-31 13:10:55 +0100

    register + login

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Volt::route('/', 'users.index');
 Route::middleware('guest')->group(function () {
+    Volt::route('/login', 'auth.login')->name('login');
     Volt::route('/register', 'auth.register');
+ 
 });
