217506751035bc24a67183abad4e6c55d81e4be8
Dasse <pascaldasse56@gmail.com>
2024-10-31 13:49:20 +0100

    add accueil

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 
 Volt::route('/', 'index');
+Volt::route('/category/{slug}', 'index');
 Route::middleware('guest')->group(function () {
     Volt::route('/login', 'auth.login')->name('login');
     Volt::route('/register', 'auth.register');
