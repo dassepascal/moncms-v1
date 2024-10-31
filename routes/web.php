8f37d779256f8e5d0dc0cd1c18135851517492dc
Dasse <pascaldasse56@gmail.com>
2024-10-31 15:44:28 +0100

    page show

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Volt::route('/', 'index');
 Volt::route('/category/{slug}', 'index');
 Volt::route('/posts/{slug}', 'posts.show')->name('posts.show');
 Volt::route('/search/{param}', 'index')->name('posts.search');
+Volt::route('/pages/{page:slug}', 'pages.show')->name('pages.show');
 Route::middleware('guest')->group(function () {
     Volt::route('/login', 'auth.login')->name('login');
     Volt::route('/register', 'auth.register');
