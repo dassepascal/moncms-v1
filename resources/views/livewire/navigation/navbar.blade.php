f6802579c3f7c473689e374829a4349015166462
Dasse <pascaldasse56@gmail.com>
2024-11-01 15:07:06 +0100

    profil

--- resources/views/livewire/navigation/navbar.blade.php
+++ resources/views/livewire/navigation/navbar.blade.php
@@ -5,8 +5,7 @@ use Livewire\Volt\Component;
 use Illuminate\Support\Collection;
 
 new class extends Component {
-
-     public Collection $menus;
+    public Collection $menus;
 
     public function mount(Collection $menus): void
     {
@@ -24,7 +23,7 @@ new class extends Component {
 };
 ?>
 
-<x-nav sticky full-width >
+<x-nav sticky full-width>
     <x-slot:brand>
         <label for="main-drawer" class="mr-3 lg:hidden">
             <x-icon name="o-bars-3" class="cursor-pointer" />
@@ -38,30 +37,31 @@ new class extends Component {
                     <x-slot:trigger>
                         <x-button label="{{ $user->name }}" class="btn-ghost" />
                     </x-slot:trigger>
+                    <x-menu-item title="{{ __('Profile') }}" link="{{ route('profile') }}" />
                     <x-menu-item title="{{ __('Logout') }}" wire:click="logout" />
                 </x-dropdown>
             @else
                 <x-button label="{{ __('Login') }}" link="/login" class="btn-ghost" />
             @endif
             @foreach ($menus as $menu)
-            @if ($menu->submenus->isNotEmpty())
-                <x-dropdown>
-                    <x-slot:trigger>
-                        <x-button label="{{ $menu->label }}" class="btn-ghost" />
-                    </x-slot:trigger>
-                    @foreach ($menu->submenus as $submenu)
-                        <x-menu-item title="{{ $submenu->label }}" link="{{ $submenu->link }}"
-                            style="min-width: max-content; bg-red-500" />
-                    @endforeach
-                </x-dropdown>
-            @else
-                <x-button label="{{ $menu->label }}" link="{{ $menu->link }}" :external="Str::startsWith($menu->link, 'http')"
-                    class="btn-ghost" />
-            @endif
-        @endforeach
+                @if ($menu->submenus->isNotEmpty())
+                    <x-dropdown>
+                        <x-slot:trigger>
+                            <x-button label="{{ $menu->label }}" class="btn-ghost" />
+                        </x-slot:trigger>
+                        @foreach ($menu->submenus as $submenu)
+                            <x-menu-item title="{{ $submenu->label }}" link="{{ $submenu->link }}"
+                                style="min-width: max-content; bg-red-500" />
+                        @endforeach
+                    </x-dropdown>
+                @else
+                    <x-button label="{{ $menu->label }}" link="{{ $menu->link }}" :external="Str::startsWith($menu->link, 'http')"
+                        class="btn-ghost" />
+                @endif
+            @endforeach
         </span>
-         
-          <x-theme-toggle title="{{ __('Toggle theme') }}" class="w-4 h-8" />
+
+        <x-theme-toggle title="{{ __('Toggle theme') }}" class="w-4 h-8" />
         <livewire:search />
     </x-slot:actions>
 </x-nav>
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ Route::middleware('guest')->group(function () {
     Volt::route('/register', 'auth.register');
     Volt::route('/forgot-password', 'auth.forgot-password');
     Volt::route('/reset-password/{token}', 'auth.reset-password')->name('password.reset');
+    
  
+});
+Route::middleware('auth')->group(function () {
+    Volt::route('/profile', 'auth.profile')->name('profile');
 });
